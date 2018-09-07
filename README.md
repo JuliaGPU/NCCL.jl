9c5eae94863198052fde3fec83e97b18654ff83e
Tim Besard <tim.besard@gmail.com>
2018-09-07 16:53:17 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+NCCL.jl
+=======
+
+*A Julia wrapper for the NVIDIA Collective Communications Library.*
+
+
+Status
+------
+
+This package is very incomplete, and completely untested.
---
+++ src/NCCL.jl
@@ -0,0 +1,13 @@
+module NCCL
+
+const ext = joinpath(dirname(@__DIR__), "deps", "ext.jl")
+isfile(ext) || error("NCCL.jl has not been built, please run Pkg.build(\"NCCL\").")
+include(ext)
+if !configured
+    # default (non-functional) values for critical variables,
+    # making it possible to _load_ the package at all times.
+    const libnccl = nothing
+end
+
+
+end
