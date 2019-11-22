b7a2427b45351b09409f1408015d9e190dc17f96
Katharine Hyatt <khyatt@flatironinstitute.org>
2019-11-22 14:58:17 -0800

    ReduceScatter and AllGather too

--- src/collective.jl
+++ src/collective.jl
@@ -20,3 +20,17 @@ function Reduce!(sendbuf, recvbuf, count::Integer, op, root::Int, comm::Communic
              sendbuf, recvbuf, count, data_type, op, root, comm.handle, stream)
     return recvbuf
 end
+
+function Allgather!(sendbuf, recvbuf, sendcount::Integer, comm::Communicator; stream::CuStream=CuDefaultStream() )
+    data_type = ncclDataType(eltype(recvbuf))
+    @apicall(:ncclAllGather, (CuPtr{Cvoid}, CuPtr{Cvoid}, Cint, ncclDataType_t, ncclComm_t, CUstream),
+             sendbuf, recvbuf, sendcount, data_type, comm.handle, stream)
+    return recvbuf
+end
+
+function ReduceScatter!(sendbuf, recvbuf, recvcount::Integer, op, comm::Communicator; stream::CuStream=CuDefaultStream() )
+    data_type = ncclDataType(eltype(recvbuf))
+    @apicall(:ncclReduceScatter, (CuPtr{Cvoid}, CuPtr{Cvoid}, Cint, ncclDataType_t, ncclRedOp_t, ncclComm_t, CUstream),
+             sendbuf, recvbuf, recvcount, data_type, op, comm.handle, stream)
+    return recvbuf
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -88,4 +88,53 @@ using Test
             @test all(crecv .== answer)
         end
     end
+    @testset "Allgather!" begin
+        devs  = CUDAdrv.devices()
+        comms = Communicator(devs)
+        recvbuf = Vector{CuVector{Float64}}(undef, length(devs))
+        sendbuf = Vector{CuVector{Float64}}(undef, length(devs))
+        streams = Vector{CuStream}(undef, length(devs))
+        for (ii, dev) in enumerate(devs)
+            CUDAnative.device!(ii - 1)
+            sendbuf[ii] = cu(fill(Float64(ii), 512))
+            recvbuf[ii] = CuArrays.zeros(Float64, length(devs)*512)
+            streams[ii] = CuStream()
+        end
+        groupStart()
+        for ii in 1:length(devs)
+            Allgather!(sendbuf[ii], recvbuf[ii], 512, comms[ii], stream=streams[ii])
+        end
+        groupEnd()
+        answer = vec(repeat(1:length(devs), inner=512))  
+        for (ii, dev) in enumerate(devs)
+            device!(ii - 1)
+            crecv = collect(recvbuf[ii])
+            @test all(crecv .== answer)
+        end
+    end
+    @testset "ReduceScatter!" begin
+        devs  = CUDAdrv.devices()
+        comms = Communicator(devs)
+        recvbuf = Vector{CuVector{Float64}}(undef, length(devs))
+        sendbuf = Vector{CuVector{Float64}}(undef, length(devs))
+        streams = Vector{CuStream}(undef, length(devs))
+        root  = 0
+        for (ii, dev) in enumerate(devs)
+            CUDAnative.device!(ii - 1)
+            sendbuf[ii] = cu(vec(repeat(collect(1:length(devs)), inner=2)))
+            recvbuf[ii] = CuArrays.zeros(Float64, length(devs))
+            streams[ii] = CuStream()
+        end
+        groupStart()
+        for ii in 1:length(devs)
+            ReduceScatter!(sendbuf[ii], recvbuf[ii], 2, NCCL.ncclSum, comms[ii], stream=streams[ii])
+        end
+        groupEnd()
+        for (ii, dev) in enumerate(devs)
+            answer = length(devs)*ii
+            device!(ii - 1)
+            crecv = collect(recvbuf[ii])
+            @test all(crecv .== answer)
+        end
+    end
 end
