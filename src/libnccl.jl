8121d956e76d79f1b50d7eeca911c0d97091b58a
Mosè Giordano <765740+giordano@users.noreply.github.com>
2026-07-10 01:01:00 +0100

    Fix compatibility with CUDA.jl v6

--- src/libnccl.jl
+++ src/libnccl.jl
@@ -8,7 +8,13 @@ using CEnum: CEnum, @cenum
 const NULL = C_NULL
 const INT_MIN = typemin(Cint)
 
-import CUDA: @checked, CuPtr, CUstream
+import CUDA
+# Compatibility for CUDA v5 and v6
+if isdefined(CUDA, :CUDACore)
+    using CUDA.CUDACore: @checked, CuPtr, CUstream
+else
+    using CUDA: @checked, CuPtr, CUstream
+end
 
 function check(f)
     res = f()::ncclResult_t
